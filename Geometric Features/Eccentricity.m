18a8cf13ce5bc3bfdfc672113f32516c21bd3654
Sabarinathan <40907627+sabaridsn@users.noreply.github.com>
2019-08-25 20:42:56 +0900

    updated regionprops

--- Geometric Features/Eccentricity.m
+++ Geometric Features/Eccentricity.m
@@ -4,7 +4,7 @@ function value = Eccentricity(BW)
 %%% A Segmentation Method and Comparison of Classification Methods for Thyroid Ultrasound Images %%%
 
 
-properties = regionprops(BW,{'MajorAxisLength','Minor Axis Length'});
+properties = regionprops(BW,{'MajorAxisLength','MinorAxisLength'});
 value= properties.MinorAxisLength / properties.MajorAxisLength ;
 
 end
