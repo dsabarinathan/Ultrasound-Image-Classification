747ce1240c94da305097aaf4659126f910bcd625
Sabarinathan <40907627+sabaridsn@users.noreply.github.com>
2019-08-31 20:53:36 +0900

    Add files via upload

--- Geometric Features/BoundingBoxCords.m
+++ Geometric Features/BoundingBoxCords.m
@@ -5,7 +5,7 @@ function [value1,value2,value3,value4] = BoundingBoxCords(BW)
  %%%
 
 
-properties = regionprops(BW,{'BoundingBox'});
+properties = regionprops(BW,'BoundingBox');
 x1 = properties.BoundingBox(1); 
 y1 = properties.BoundingBox(2); 
 x2 = properties.BoundingBox(2); 
@@ -13,7 +13,7 @@ y2 = properties.BoundingBox(2);
 x3 = properties.BoundingBox(3); 
 y3 = properties.BoundingBox(4); 
 x4 = properties.BoundingBox(4); 
-y4 = properties.BoundingBox(4);) 
+y4 = properties.BoundingBox(4); 
 d1 = sqrt((x1-x2)^2*(y1-y2)^2); 
 d2 = sqrt((x3-x4)^2*(y3-y4)^2) ;
 d3 = sqrt((x4-x2)^2*(y4-y2)^2); 
